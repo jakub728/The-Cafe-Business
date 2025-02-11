fe5dafbc3d354cbaeda161510e3d7f17d348e416
Student <saveenyadav28@gmail.com>
2025-02-11 11:30:51 +0100

    cafe and main updated

---
+++ cafe.js
@@ -0,0 +1,14 @@
+export class Cafe{
+    constructor(branchId,name,city,sellsFood){
+        this.branchId=branchId;
+        this.name=name;
+        this.city=city;
+        this.sellsFood=sellsFood;
+        }
+            employee=[];
+            menu=[];
+            order=[]
+        }
+
+const cafe1= new Cafe(1,"Brown House","Berlin",false);
+console.log(cafe1);
